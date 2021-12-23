2975cde4e455bcdb2d95566c008fcc207049fe45
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-23 15:31:47 +0530

    latest

---
+++ src/linecomparison/LineComparison.java
@@ -0,0 +1,11 @@
+package linecomparison;
+import java.util.*;
+import java.lang.Math;
+public class LineComparison {
+    public  static void  main(String[] args)
+    {
+
+        System.out.println("Welcome To Line Comparison");
+
+    }
+}
---
+++ src/linecomparison/Main.java
@@ -0,0 +1,8 @@
+package linecomparison;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
