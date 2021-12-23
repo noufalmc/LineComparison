c0a6f6eb901c44a0604e5a9b022e7dab81c4f92a
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-23 15:39:17 +0530

    added:user input and store

--- src/linecomparison/LineComparison.java
+++ src/linecomparison/LineComparison.java
@@ -4,8 +4,13 @@ import java.lang.Math;
 public class LineComparison {
     public  static void  main(String[] args)
     {
-
+        Scanner sc=new Scanner(System.in);
         System.out.println("Welcome To Line Comparison");
-
+        System.out.println("Enter the X1 and X2");
+        int x1=sc.nextInt();
+        int x2=sc.nextInt();
+        System.out.println("Enter the Y1 and Y2");
+        int y1=sc.nextInt();
+        int y2=sc.nextInt();
     }
 }
